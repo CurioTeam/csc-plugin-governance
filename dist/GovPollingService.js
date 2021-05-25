3043af5dae86286bad47e9e0ca109c9f05fc331d
Yuriy Novikov <y.novikov@rebase.agency>
2021-05-25 17:39:40 +0300

    chore: rebuild plugin

--- dist/GovPollingService.js
+++ dist/GovPollingService.js
@@ -57,10 +57,11 @@ function ownKeys(object, enumerableOnly) {
   var keys = Object.keys(object);
   if (Object.getOwnPropertySymbols) {
     var symbols = Object.getOwnPropertySymbols(object);
-    if (enumerableOnly)
+    if (enumerableOnly) {
       symbols = symbols.filter(function (sym) {
         return Object.getOwnPropertyDescriptor(object, sym).enumerable;
       });
+    }
     keys.push.apply(keys, symbols);
   }
   return keys;
@@ -1034,13 +1035,11 @@ var GovPollingService = /*#__PURE__*/ (function (_PrivateService) {
 
                         votesToBeMoved.forEach(function (vote) {
                           var prevChoice = votes[vote.index].choice;
-                          votes[vote.index].choice = votes[
-                            vote.index
-                          ].ballot.pop();
+                          votes[vote.index].choice =
+                            votes[vote.index].ballot.pop();
                           if (!tally.options[votes[vote.index].choice])
-                            tally.options[
-                              votes[vote.index].choice
-                            ] = _objectSpread({}, defaultOptionObj);
+                            tally.options[votes[vote.index].choice] =
+                              _objectSpread({}, defaultOptionObj);
                           tally.options[votes[vote.index].choice].transfer = (0,
                           _bignumber['default'])(
                             tally.options[votes[vote.index].choice].transfer
@@ -1400,9 +1399,8 @@ var GovPollingService = /*#__PURE__*/ (function (_PrivateService) {
                           var option = parseInt(log.topics[3]);
                           var rankedChoiceOption = [];
                           if (option > 100)
-                            rankedChoiceOption = _this._decodeRankedChoiceOptions(
-                              log.topics[3]
-                            );
+                            rankedChoiceOption =
+                              _this._decodeRankedChoiceOptions(log.topics[3]);
                           polls.push({
                             pollId: parseInt(log.topics[2]),
                             option: option,
